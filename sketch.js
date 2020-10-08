45c0184ee553b50487da0f50938eac1096dcee65
TheNerdyCooki <65473418+TheNerdyCooki@users.noreply.github.com>
2020-10-08 18:33:33 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    pla = new Ground(200, 600, 100, 400)
+    pla = new Ground(200, 400, 100, 400)
 
     bird = new Bird(100,100);
 
